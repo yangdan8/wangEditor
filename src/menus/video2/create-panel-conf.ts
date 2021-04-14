8dcee673f5c8dd64111581d6e3ac16fab5b56973
hahaaha <390519764@qq.com>
2021-04-14 18:31:18 +0800

    refator: 增加视频条件判断

--- src/menus/video2/create-panel-conf.ts
+++ src/menus/video2/create-panel-conf.ts
@@ -7,6 +7,34 @@ export default function (editor: Editor) {
     const inputId = getRandom('input')
     const btnOkId = getRandom('btnOkId')
 
+    /**
+     * 插入链接
+     * @param iframe html标签
+     */
+    function insertVideo(video: string): void {
+        editor.cmd.insertEmbed('video2', video)
+
+        // video添加后的回调
+        editor.config.onlineVideoCallback(video)
+    }
+
+    /**
+    * 校验在线视频链接
+    * @param video 在线视频链接
+    */
+    function checkOnlineVideo(video: string): boolean {
+        // 查看开发者自定义配置的返回值
+        const check = editor.config.onlineVideoCheck(video)
+        if (check === true) {
+            return true
+        }
+        if (typeof check === 'string') {
+            //用户未能通过开发者的校验，开发者希望我们提示这一字符串
+            editor.config.customAlert(check, 'error')
+        }
+        return false
+    }
+
     const conf: PanelConf = {
         width: 300,
         height: 0,
@@ -20,11 +48,14 @@ export default function (editor: Editor) {
                     <div>
                         <input id="${inputId}"
                             type="text"
+                            class="block"
                             placeholder="请输入视频地址"
                             >
                     </div>
-                    <div>
-                        <button type="button" id="${btnOkId}">insert</button>
+                    <div class="w-e-button-container">
+                        <button type="button" id="${btnOkId}">
+                            ${editor.i18next.t('插入')}
+                        </button>
                     </div>
                 </div>
             `,
@@ -35,9 +66,17 @@ export default function (editor: Editor) {
                     fn: () => {
                         const $input = $('#' + inputId)
                         console.log($input)
-                        const value = $input.val()
-                        editor.cmd.insertEmbed('video2', value)
+                        const video = $input.val().trim()
 
+                        // 视频为空 不插入
+                        if (!video) return
+
+                        // 对当前用户插入的内容进行判断，插入为空，或者返回false，都停止插入
+                        if (!checkOnlineVideo(video)) return
+
+                        insertVideo(video)
+
+                        // return true panel关闭
                         return true
                     }
                 }
